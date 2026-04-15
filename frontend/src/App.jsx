17caf9af98817c6026301749d2df1ec214384b2b
YASH <yasarreza456@gmail.com>
2026-04-15 19:47:01 +0530

    Add SpeedInsights component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { AppRoutes } from './app/routes/AppRoutes.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
-export default function App(){
-  return <AppRoutes />
+export default function App() {
+  return (
+    <>
+      <AppRoutes />
+      <SpeedInsights />
+    </>
+  )
 }
